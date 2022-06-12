0b65d2644b70c0e21563e50e9e5c93bd903ae114
Shimamura-Seiya <shimamura.seiya@classmethod.jp>
2022-06-12 14:49:19 +0900

    Add Rename ContentView to MainView and Add SecondView

--- GitTraining/GitTrainingApp.swift
+++ GitTraining/GitTrainingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GitTrainingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ GitTraining/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitTraining
+//
+//  Created by shimamura.seiya on 2022/06/12.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
