b16da215a71bcbe8a1bc7d54d31f52c79b095c1a
Shimamura-Seiya <shimamura.seiya@classmethod.jp>
2022-06-12 14:15:12 +0900

    Initial Commit

---
+++ GitTraining/GitTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrainingApp.swift
+//  GitTraining
+//
+//  Created by shimamura.seiya on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct GitTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
